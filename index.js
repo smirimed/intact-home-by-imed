4bbaef2d366cddfda39d78366139c0cbcf9e6388
Smiri Imed <118073749+smirimed@users.noreply.github.com>
2024-04-05 05:10:03 +0100

    final

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ window.onscroll = () => {
 
 };
 
+let paragraphe = document.querySelector(".paragraph");
+let button = document.querySelector(".toggle-button");
+button.addEventListener("click",function () {
+    paragraphe.classList.toggle("hide")
+  })
+let paragraphe1 = document.querySelector(".paragraph1");
+let button1 = document.querySelector(".toggle-button1");
+button1.addEventListener("click",function () {
+    paragraphe1.classList.toggle("hide")
+  })
+let paragraphe2 = document.querySelector(".paragraph2");
+let button2 = document.querySelector(".toggle-button2");
+button2.addEventListener("click",function () {
+    paragraphe2.classList.toggle("hide")
+  })
