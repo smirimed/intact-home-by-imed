a816f35af5e2eb4062863fab45d99df9d03412b5
Smiri Imed <118073749+smirimed@users.noreply.github.com>
2024-04-05 03:29:02 +0100

    changement ghaith

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 let menu = document.querySelector(".menu-icon");
 let navbar = document.querySelector(".navbar");
+let filter =document.querySelector(".filter")
 
 menu.addEventListener("click", function () {
     navbar.classList.toggle("active");
+    filter.classList.toggle("filtred");
+    
+    
+
 });
 
 window.onscroll = () => {
     navbar.classList.remove("active");
+    filter.classList.remove("filtred");
+
 };
 
